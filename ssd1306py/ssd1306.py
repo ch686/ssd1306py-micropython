f98398397d4c2dad707bc36cdcd103e93c608c17
ch686 <54013203+ch686@users.noreply.github.com>
2022-01-21 14:59:10 +0800

    Update ssd1306.py
    驱动内置 set_myfont  draw_text 可以显示英文加汉字

--- ssd1306py/ssd1306.py
+++ ssd1306py/ssd1306.py
@@ -1,167 +1,204 @@
-#MicroPython SSD1306 OLED driver, I2C and SPI interfaces created by Adafruit
-
-import time
 import framebuf
+SSD1306_I2C_ADDR = 0x3C
 
 # register definitions
-SET_CONTRAST        = const(0x81)
-SET_ENTIRE_ON       = const(0xa4)
-SET_NORM_INV        = const(0xa6)
-SET_DISP            = const(0xae)
-SET_MEM_ADDR        = const(0x20)
-SET_COL_ADDR        = const(0x21)
-SET_PAGE_ADDR       = const(0x22)
+SET_CONTRAST = const(0x81)
+SET_ENTIRE_ON = const(0xA4)
+SET_NORM_INV = const(0xA6)
+SET_DISP = const(0xAE)
+SET_MEM_ADDR = const(0x20)
+SET_COL_ADDR = const(0x21)
+SET_PAGE_ADDR = const(0x22)
 SET_DISP_START_LINE = const(0x40)
-SET_SEG_REMAP       = const(0xa0)
-SET_MUX_RATIO       = const(0xa8)
-SET_COM_OUT_DIR     = const(0xc0)
-SET_DISP_OFFSET     = const(0xd3)
-SET_COM_PIN_CFG     = const(0xda)
-SET_DISP_CLK_DIV    = const(0xd5)
-SET_PRECHARGE       = const(0xd9)
-SET_VCOM_DESEL      = const(0xdb)
-SET_CHARGE_PUMP     = const(0x8d)
+SET_SEG_REMAP = const(0xA0)
+SET_MUX_RATIO = const(0xA8)
+SET_IREF_SELECT = const(0xAD)
+SET_COM_OUT_DIR = const(0xC0)
+SET_DISP_OFFSET = const(0xD3)
+SET_COM_PIN_CFG = const(0xDA)
+SET_DISP_CLK_DIV = const(0xD5)
+SET_PRECHARGE = const(0xD9)
+SET_VCOM_DESEL = const(0xDB)
+SET_CHARGE_PUMP = const(0x8D)
 
+# Subclassing FrameBuffer provides support for graphics primitives
+# http://docs.micropython.org/en/latest/pyboard/library/framebuf.html
+class SSD1306(framebuf.FrameBuffer):
+	def __init__(self, width, height, external_vcc):
+		self._fonts = dict()
+		self.width = width
+		self.height = height
+		self.external_vcc = external_vcc
+		self.pages = self.height // 8
+		self.buffer = bytearray(self.pages * self.width)
+		super().__init__(self.buffer, self.width, self.height, framebuf.MONO_VLSB)
+		self.init_display()
+	def init_display(self):
+		for cmd in (
+			SET_DISP,  # display off
+			# address setting
+			SET_MEM_ADDR,
+			0x00,  # horizontal
+			# resolution and layout
+			SET_DISP_START_LINE,  # start at line 0
+			SET_SEG_REMAP | 0x01,  # column addr 127 mapped to SEG0
+			SET_MUX_RATIO,
+			self.height - 1,
+			SET_COM_OUT_DIR | 0x08,  # scan from COM[N] to COM0
+			SET_DISP_OFFSET,
+			0x00,
+			SET_COM_PIN_CFG,
+			0x02 if self.width > 2 * self.height else 0x12,
+			# timing and driving scheme
+			SET_DISP_CLK_DIV,
+			0x80,
+			SET_PRECHARGE,
+			0x22 if self.external_vcc else 0xF1,
+			SET_VCOM_DESEL,
+			0x30,  # 0.83*Vcc
+			# display
+			SET_CONTRAST,
+			0xFF,  # maximum
+			SET_ENTIRE_ON,  # output follows RAM contents
+			SET_NORM_INV,  # not inverted
+			SET_IREF_SELECT,
+			0x30,  # enable internal IREF during display on
+			# charge pump
+			SET_CHARGE_PUMP,
+			0x10 if self.external_vcc else 0x14,
+			SET_DISP | 0x01,  # display on
+		):  # on
+			self.write_cmd(cmd)
+		self.fill(0)
+		self.show()
+	def poweroff(self):
+		self.write_cmd(SET_DISP)
+	def poweron(self):
+		self.write_cmd(SET_DISP | 0x01)
 
-class SSD1306:
-    def __init__(self, width, height, external_vcc):
-        self.width = width
-        self.height = height
-        self.external_vcc = external_vcc
-        self.pages = self.height // 8
-        # Note the subclass must initialize self.framebuf to a framebuffer.
-        # This is necessary because the underlying data buffer is different
-        # between I2C and SPI implementations (I2C needs an extra byte).
-        self.poweron()
-        self.init_display()
+	def contrast(self, contrast):
+		self.write_cmd(SET_CONTRAST)
+		self.write_cmd(contrast)
 
-    def init_display(self):
-        for cmd in (
-            SET_DISP | 0x00, # off
-            # address setting
-            SET_MEM_ADDR, 0x00, # horizontal
-            # resolution and layout
-            SET_DISP_START_LINE | 0x00,
-            SET_SEG_REMAP | 0x01, # column addr 127 mapped to SEG0
-            SET_MUX_RATIO, self.height - 1,
-            SET_COM_OUT_DIR | 0x08, # scan from COM[N] to COM0
-            SET_DISP_OFFSET, 0x00,
-            SET_COM_PIN_CFG, 0x02 if self.height == 32 else 0x12,
-            # timing and driving scheme
-            SET_DISP_CLK_DIV, 0x80,
-            SET_PRECHARGE, 0x22 if self.external_vcc else 0xf1,
-            SET_VCOM_DESEL, 0x30, # 0.83*Vcc
-            # display
-            SET_CONTRAST, 0xff, # maximum
-            SET_ENTIRE_ON, # output follows RAM contents
-            SET_NORM_INV, # not inverted
-            # charge pump
-            SET_CHARGE_PUMP, 0x10 if self.external_vcc else 0x14,
-            SET_DISP | 0x01): # on
-            self.write_cmd(cmd)
-        self.fill(0)
-        self.show()
+	def invert(self, invert):
+		self.write_cmd(SET_NORM_INV | (invert & 1))
 
-    def poweroff(self):
-        self.write_cmd(SET_DISP | 0x00)
-
-    def contrast(self, contrast):
-        self.write_cmd(SET_CONTRAST)
-        self.write_cmd(contrast)
-
-    def invert(self, invert):
-        self.write_cmd(SET_NORM_INV | (invert & 1))
-
-    def show(self):
-        x0 = 0
-        x1 = self.width - 1
-        if self.width == 64:
-            # displays with width of 64 pixels are shifted by 32
-            x0 += 32
-            x1 += 32
-        self.write_cmd(SET_COL_ADDR)
-        self.write_cmd(x0)
-        self.write_cmd(x1)
-        self.write_cmd(SET_PAGE_ADDR)
-        self.write_cmd(0)
-        self.write_cmd(self.pages - 1)
-        self.write_framebuf()
-
-    def fill(self, col):
-        self.framebuf.fill(col)
-
-    def pixel(self, x, y, col):
-        self.framebuf.pixel(x, y, col)
-
-    def scroll(self, dx, dy):
-        self.framebuf.scroll(dx, dy)
-
-    def text(self, string, x, y, col=1):
-        self.framebuf.text(string, x, y, col)
-
+	def rotate(self, rotate):
+		self.write_cmd(SET_COM_OUT_DIR | ((rotate & 1) << 3))
+		self.write_cmd(SET_SEG_REMAP | (rotate & 1))
+	
+	def show(self):
+		x0 = 0
+		x1 = self.width - 1
+		if self.width != 128:
+			# narrow displays use centred columns
+			col_offset = (128 - self.width) // 2
+			x0 += col_offset
+			x1 += col_offset
+		self.write_cmd(SET_COL_ADDR)
+		self.write_cmd(x0)
+		self.write_cmd(x1)
+		self.write_cmd(SET_PAGE_ADDR)
+		self.write_cmd(0)
+		self.write_cmd(self.pages - 1)
+		self.write_data(self.buffer)
+	
+	def set_myfont(self,font, font_size):
+		"""设置字库"""	
+		if font_size not in [8,16,24,32]:
+			return
+		self._fonts[font_size] = font
+		
+	def draw_text(self,string, x_axis, y_axis, font_size=8):
+		offset = 0	
+		for k in string:			
+			data_code = k.encode("utf-8")	
+			code = int.from_bytes(data_code, 'big', False)
+			byte_data=list(bytearray(font_size//8*font_size))			
+			for i in range (0,len(byte_data)):
+				if i%2==0:
+					byte_data[i]=0xaa
+				else:
+					byte_data[i]=0x55
+			if font_size in self._fonts:
+				if code in self._fonts[font_size]:
+					byte_data = self._fonts[font_size][code]
+			
+			if len(data_code)==3: #中文				
+				fb= framebuf.FrameBuffer(bytearray(byte_data), font_size, font_size, framebuf.MONO_VLSB)				
+				self.blit(fb,x_axis + offset,y_axis)
+				offset += font_size				
+			else:				#英文符号
+				if byte_data[0]==0xaa and byte_data[1]==0x55 and font_size==8:
+					self.text(k, x_axis+ offset,y_axis)
+					offset += font_size
+				else:	
+					fb= framebuf.FrameBuffer(bytearray(byte_data), font_size//2, font_size, framebuf.MONO_VLSB)					
+					self.blit(fb,x_axis + offset,y_axis)				
+					offset += font_size//2							
 
 class SSD1306_I2C(SSD1306):
-    def __init__(self, width, height, i2c, addr=0x3c, external_vcc=False):
-        self.i2c = i2c
-        self.addr = addr
-        self.temp = bytearray(2)
-        # Add an extra byte to the data buffer to hold an I2C data/command byte
-        # to use hardware-compatible I2C transactions.  A memoryview of the
-        # buffer is used to mask this byte from the framebuffer operations
-        # (without a major memory hit as memoryview doesn't copy to a separate
-        # buffer).
-        self.buffer = bytearray(((height // 8) * width) + 1)
-        self.buffer[0] = 0x40  # Set first byte of data buffer to Co=0, D/C=1
-        self.framebuf = framebuf.FrameBuffer1(memoryview(self.buffer)[1:], width, height)
-        super().__init__(width, height, external_vcc)
-
-    def write_cmd(self, cmd):
-        self.temp[0] = 0x80 # Co=1, D/C#=0
-        self.temp[1] = cmd
-        self.i2c.writeto(self.addr, self.temp)
-
-    def write_framebuf(self):
-        # Blast out the frame buffer using a single I2C transaction to support
-        # hardware I2C interfaces.
-        self.i2c.writeto(self.addr, self.buffer)
-
-    def poweron(self):
-        pass
-
-
-class SSD1306_SPI(SSD1306):
-    def __init__(self, width, height, spi, dc, res, cs, external_vcc=False):
-        self.rate = 10 * 1024 * 1024
-        dc.init(dc.OUT, value=0)
-        res.init(res.OUT, value=0)
-        cs.init(cs.OUT, value=1)
-        self.spi = spi
-        self.dc = dc
-        self.res = res
-        self.cs = cs
-        self.buffer = bytearray((height // 8) * width)
-        self.framebuf = framebuf.FrameBuffer1(self.buffer, width, height)
-        super().__init__(width, height, external_vcc)
-
-    def write_cmd(self, cmd):
-        self.spi.init(baudrate=self.rate, polarity=0, phase=0)
-        self.cs.high()
-        self.dc.low()
-        self.cs.low()
-        self.spi.write(bytearray([cmd]))
-        self.cs.high()
-
-    def write_framebuf(self):
-        self.spi.init(baudrate=self.rate, polarity=0, phase=0)
-        self.cs.high()
-        self.dc.high()
-        self.cs.low()
-        self.spi.write(self.buffer)
-        self.cs.high()
-
-    def poweron(self):
-        self.res.high()
-        time.sleep_ms(1)
-        self.res.low()
-        time.sleep_ms(10)
-        self.res.high()
+	def __init__(self,i2c, width, height,external_vcc=False):
+		self.i2c=i2c
+		self.addr = SSD1306_I2C_ADDR		
+		self.temp = bytearray(2)
+		self.write_list = [b"\x40", None]  # Co=0, D/C#=1
+		if self.is_connect()==1:
+			super().__init__(width, height, external_vcc)
+	
+	def	is_connect(self):		
+		addresses=self.i2c.scan()
+		bfind=0
+		for addr in addresses:
+			if addr==SSD1306_I2C_ADDR:
+				bfind=1				
+				break
+		return bfind
+		
+	def write_cmd(self, cmd):
+		self.temp[0] = 0x80  # Co=1, D/C#=0
+		self.temp[1] = cmd
+		try:
+			self.i2c.writeto(self.addr, self.temp)
+		except:
+			return
+			
+	def write_data(self, buf):
+		self.write_list[1] = buf
+		try:
+			self.i2c.writevto(self.addr, self.write_list)
+		except:
+			return
+			
+class SSD1306_pybI2C(SSD1306):
+	def __init__(self,i2c, width, height, external_vcc=False):
+		self.i2c =i2c
+		self.addr = SSD1306_I2C_ADDR		
+		self.temp = bytearray(2)
+		self.write_list = [b"\x40", None]  # Co=0, D/C#=1
+		if self.is_connect()==1:
+			super().__init__(width, height, external_vcc)
+	
+	def	is_connect(self):		
+		addresses=self.i2c.scan()		
+		bfind=0
+		for addr in addresses:
+			if addr==SSD1306_I2C_ADDR:
+				bfind=1				
+				break
+		return bfind
+		
+	def write_cmd(self, cmd):
+		self.temp[0] = 0x80  # Co=1, D/C#=0
+		self.temp[1] = cmd
+		try:
+			self.i2c.send(self.temp,self.addr)
+		except:
+			return
+			
+	def write_data(self, buf):
+		self.write_list[1] = buf
+		try:
+			self.i2c.send(self.write_list,self.addr)
+		except:
+			return			
